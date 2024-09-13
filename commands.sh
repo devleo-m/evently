50aec41c072829814d4a16b7574aff5ca1bdb707
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-13 19:22:59 -0300

    initial project structure created

---
+++ commands.sh
@@ -0,0 +1,13 @@
+npm init -y
+npm install express body-parser express-validator multer pg sequelize typescript @types/node @types/express ts-node
+npm install express sequelize pg pg-hstore bcryptjs jsonwebtoken body-parser dotenv
+npm install typescript --save-dev
+npm install typescript ts-node @types/express @types/node --save-dev
+npm install --save-dev nodemon @types/bcryptjs @types/jsonwebtoken @types/body-parser
+npx tsc --init
+
+#Instalar Jest e Supertest
+npm install --save-dev jest supertest ts-jest @types/jest @types/supertest
+npm install --save-dev @testing-library/jest-dom
+
+npm install --save sequelize-cli
---
+++ src/web/server.ts
@@ -0,0 +1,18 @@
+import bodyParser from "body-parser";
+import express from "express";
+import sequelize from "../config/database";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+sequelize.sync()
+    .then(() => {
+        console.log('Banco de dados conectado com sucesso!');
+        app.listen(port, () => {
+        console.log(`Servidor iniciado em http://localhost:${port}`);
+        });
+}).catch(err => {
+    console.error('Erro ao conectar ao banco de dados', err);
+});
