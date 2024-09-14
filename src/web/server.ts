7d3d3ef297cafbaae87957cd48069363e18e2c62
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-14 19:22:18 -0300

    Update server.ts

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import dotenv from 'dotenv';
-import { connectDatabase } from '../database/config/database';
+import { connectDatabase } from '../config/database';
 
 dotenv.config();
 const PORT = process.env.PORT;
