74d5d48f4c97685bca3736c1c5ee85721c8da7a9
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-14 19:22:13 -0300

    Create 20240914214711-user.js

---
+++ src/database/migrations/20240914214711-user.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('User', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('User');
+  }
+};
