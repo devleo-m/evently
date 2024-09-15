897bf46bb4d912708b065a2ce828873d8f708cf0
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-15 03:26:10 -0300

    Update eventController.ts

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -15,12 +15,12 @@ export namespace eventController {
   export const createEvent = async (req: Request, res: Response) => {
     try {
       const parsedData = eventSchema.parse(req.body);
-      const [day, month, year] = parsedData.event_date.split('/');
+      const [day, month, year] = parsedData.event_date.split("/");
       const formattedDate = `${year}-${month}-${day}`;
-      
+
       const event = await Event.create({
         ...parsedData,
-        event_date: formattedDate
+        event_date: formattedDate,
       });
 
       res.status(201).json(event);
@@ -48,16 +48,17 @@ export namespace eventController {
   };
 
   export const updateEvent = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
     try {
       const parsedData = eventSchema.parse(req.body);
-      const [updated] = await Event.update(parsedData, {
-        where: { id: req.params.id },
-      });
+      const [updated] = await Event.update(parsedData, { where: { id } });
+
       if (updated) {
-        const updatedEvent = await Event.findByPk(req.params.id);
-        res.json(updatedEvent);
+        const updatedEvent = await Event.findByPk(id);
+        return res.json(updatedEvent);
       } else {
-        res.status(404).json({ message: "Evento não encontrado" });
+        return res.status(404).json({ message: "Evento não encontrado" });
       }
     } catch (error) {
       if (error instanceof z.ZodError) {
@@ -65,7 +66,7 @@ export namespace eventController {
           .status(400)
           .json({ message: "Dados inválidos", errors: error.errors });
       }
-      res.status(500).json({ message: "Erro ao atualizar evento" });
+      return res.status(500).json({ message: "Erro ao atualizar evento" });
     }
   };
 
@@ -87,21 +88,24 @@ export namespace eventController {
 
 // verifica se uma data é válida / padronizando o formato DD/MM/YYYY
 const isValidDate = (dateString: string): boolean => {
-    const regex = /^([0-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/;
-    if (!regex.test(dateString)) return false;
-  
-    const [day, month, year] = dateString.split('/').map(Number);
-    const date = new Date(year, month - 1, day);
-  
-    return date.getDate() === day && date.getMonth() === month - 1 && date.getFullYear() === year;
+  const regex = /^([0-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/;
+  if (!regex.test(dateString)) return false;
+
+  const [day, month, year] = dateString.split("/").map(Number);
+  const date = new Date(year, month - 1, day);
+
+  return (
+    date.getDate() === day &&
+    date.getMonth() === month - 1 &&
+    date.getFullYear() === year
+  );
 };
 
 const eventSchema = z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    event_date: z.string().refine(isValidDate, {
-      message: "Data inválida, use o formato DD/MM/YYYY",
-    }),
-    creator_id: z.number()
+  title: z.string(),
+  description: z.string().optional(),
+  event_date: z.string().refine(isValidDate, {
+    message: "Data inválida, use o formato DD/MM/YYYY",
+  }),
+  creator_id: z.number(),
 });
-  
