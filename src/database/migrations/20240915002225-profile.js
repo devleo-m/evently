13f1aad6c0caa7cd82cb613dbe31935f5149db49
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-14 22:28:09 -0300

    Create 20240915002225-profile.js

---
+++ src/database/migrations/20240915002225-profile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Profile', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      bio: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      birth_date: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profile');
+  }
+};
