0150e1c05e56fa20acea8d576d9ed8947c2f55ab
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-16 03:25:32 -0300

    Update app.ts

--- src/web/app.ts
+++ src/web/app.ts
@@ -4,6 +4,7 @@ import userRouter from '../routes/userRouter';
 import profileRouter from '../routes/profileRoutes';
 import eventRoutes from '../routes/eventRoutes';
 import participationRoutes from '../routes/participationRoutes';
+import authRoutes from '../routes/authRoutes';
 
 import swaggerDocument from './swagger.json';
 import swaggerUi from 'swagger-ui-express';
@@ -17,6 +18,7 @@ app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //Rotas
+app.use("/", authRoutes);
 app.use("/api", userRouter);
 app.use("/api", profileRouter);
 app.use("/api", eventRoutes);
