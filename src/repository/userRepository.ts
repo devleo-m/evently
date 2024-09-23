bbcaf6a5ab646cf5a3025a26a76339b500e9a4fb
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-23 00:12:36 -0300

    Create userRepository.ts

---
+++ src/repository/userRepository.ts
@@ -0,0 +1,61 @@
+import User, { UserAttributes, UserCreationAttributes } from "../database/models/user";
+
+class UserRepository{
+    async createUser(user: UserCreationAttributes): Promise<User> {
+        try {
+            return await User.create(user);
+        } catch (error) {
+            throw new Error(`Failed to create user ${error}`);
+        }
+    }
+
+    async findAllUsers(): Promise<User[]> {
+        try {
+            return await User.findAll();
+        } catch (error) {
+            throw new Error(`Failed to retrieve users ${error}`);
+        }
+    }
+
+    async findByIdUser(id: number): Promise<User | null> {
+        try {
+            return await User.findByPk(id);
+        } catch (error) {
+            throw new Error(`Failed to retrieve user ${error}`);
+        }    
+    }
+
+    async findByEmailUser(email: string): Promise<User | null> {
+        try {
+            return await User.findOne({ where: { email } });
+        } catch (error) {
+            throw new Error(`Failed to retrieve user ${error}`);
+        }
+    }
+
+    async updateUser(id: number, user: Partial<UserAttributes>): Promise<User | null> {
+        try {
+            const existingUser = await this.findByIdUser(id);
+            if (!existingUser) {
+                throw new Error(`User with id:${id} not found`)
+            }
+            return await existingUser.update(user);
+        } catch (error) {
+            throw new Error(`Failed to update user ${error}`);
+        }
+    }
+
+    async deleteUser(id: number): Promise<void> {
+        try {
+            const existingUser = await this.findByIdUser(id);
+            if (!existingUser) {
+                throw new Error(`User with id ${id} not found`);
+            }
+            await existingUser.destroy();
+        } catch (error) {
+            throw new Error(`Failed to delete user ${error}`);
+        }
+    }
+}
+
+export default new UserRepository();
