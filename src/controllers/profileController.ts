826f4673b88705650c5308beb52cf7eefc971611
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-23 01:43:24 -0300

    Update Controllers - Complet

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -1,86 +1,85 @@
 import { Request, Response } from 'express';
-import Profile from '../database/models/profile';
-import { z } from 'zod';
+import { z, ZodError } from 'zod';
+import profileService from '../service/profileService';
 
 const profileSchema = z.object({
   name: z.string().min(1),
   bio: z.string().optional(),
-  birth_date: z.string().optional(),
+  birth_date: z.coerce.date().min(new Date('1900-01-01')),
   user_id: z.number().int(),
 });
 
-export namespace profileController {
-  export const getProfiles = async (req: Request, res: Response) => {
-    try {
-      const profiles = await Profile.findAll();
-      res.json(profiles);
-    } catch (error) {
-      res.status(500).json({ message: 'Erro ao buscar perfis' });
-    }
-  };
+const profileUpdateSchema = z.object({
+  name: z.string().min(1).optional(),
+  bio: z.string().optional(),
+  birth_date: z.coerce.date().min(new Date('1900-01-01')).optional(),
+  user_id: z.number().int().optional(),
+});
 
+export const IdSchema = z.object({
+  id: z.coerce.number().int().positive(),
+})
+
+export namespace profileController {
   export const createProfile = async (req: Request, res: Response) => {
     try {
       const parsedData = profileSchema.parse(req.body);
-      const profile = await Profile.create(parsedData);
+      const profile = await profileService.createProfile(parsedData);
       res.status(201).json(profile);
     } catch (error) {
-      if (error instanceof z.ZodError) {
-        return res
-          .status(400)
-          .json({ message: 'Dados inválidos', errors: error.errors });
+      if (error instanceof ZodError) {
+        return res.status(400).json({ message: `Dados Invalidos ${error}` });
       }
-      res.status(500).json({ message: 'Erro ao criar perfil' });
+      res.status(500).json({ message: "Erro ao criar perfil" });
     }
   };
 
-  export const getProfileById = async (req: Request, res: Response) => {
+  export const findAllProfiles = async (req: Request, res: Response) => {
     try {
-      const profile = await Profile.findByPk(req.params.id);
-      if (profile) {
-        res.json(profile);
-      } else {
-        res.status(404).json({ message: 'Perfil não encontrado' });
-      }
+         const profiles = await profileService.findAllProfiles();
+         return res.status(200).json(profiles);
     } catch (error) {
-      res.status(500).json({ message: 'Erro ao buscar perfil' });
+         return res.status(400).json({ message: `Erro ao buscar usuários: ${error}` });
     }
-  };
+}
 
-  export const updateProfile = async (req: Request, res: Response) => {
-    try {
-      const parsedData = profileSchema.parse(req.body);
-      const [updated] = await Profile.update(parsedData, {
-        where: { id: req.params.id },
-      });
-      if (updated) {
-        const updatedProfile = await Profile.findByPk(req.params.id);
-        res.json(updatedProfile);
-      } else {
-        res.status(404).json({ message: 'Perfil não encontrado' });
-      }
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return res
-          .status(400)
-          .json({ message: 'Dados inválidos', errors: error.errors });
-      }
-      res.status(500).json({ message: 'Erro ao atualizar perfil' });
-    }
-  };
+export const findProfileById = async (req: Request, res: Response):Promise<Response> => {
+  try {
+       const idParam = IdSchema.parse(req.params);
+       const profile = await profileService.findProfileById(idParam.id);
+       return res.status(200).json(profile);
+  } catch (error) {
+       if (error instanceof ZodError) {
+        return res.status(400).json({ message: `Dados Invalidos ${error}` });
+       }
+       return res.status(400).json({ message: `Error finding profile: ${error}` });
+  }
+}
 
-  export const deleteProfile = async (req: Request, res: Response) => {
-    try {
-      const deleted = await Profile.destroy({
-        where: { id: req.params.id },
-      });
-      if (deleted) {
-        res.json({ message: 'Perfil deletado com sucesso' });
-      } else {
-        res.status(404).json({ message: 'Perfil não encontrado' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: 'Erro ao deletar perfil' });
+export const updateProfile = async (req: Request, res: Response):Promise<Response> => {
+   try {
+       const idParam = IdSchema.parse(req.params);
+       const profile = profileUpdateSchema.parse(req.body);
+       const updateProfile = await profileService.updateProfile(idParam.id, profile);
+       return res.status(200).json(updateProfile);
+   } catch (error) {
+       if (error instanceof ZodError) {
+        return res.status(400).json({ message: `Dados Invalidos ${error}` });
+       }
+       return res.status(400).json({ message: `Error updating profile: ${error}` });
+   }
+}
+
+export const deleteProfile = async (req: Request, res: Response):Promise<Response> => {
+   try {
+       const idParam = IdSchema.parse(req.params);
+       const deleteProfile = await profileService.deleteProfile(idParam.id);
+       return res.status(200).json({ message: `Profile deleted successfully`, profile: deleteProfile });
+   } catch (error) {
+       if (error instanceof ZodError) {
+        return res.status(400).json({ message: `Dados Invalidos ${error}` });
+       }
+       return res.status(400).json({ message: `Error deleting profile: ${error}` });
     }
-  };
+  }
 }
